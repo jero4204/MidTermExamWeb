1a81e2996ac773c1975d832cc23eb6754ae6c34f
Jero4204 <jeronimo.sanchez99@eia.edu.co>
2024-09-11 23:08:22 -0500

    Arregla los botones y mejora el login

--- src/components/InicioSesion.tsx
+++ src/components/InicioSesion.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Hook para redirigir
+import { User } from '../models/users';
+import { usersData } from '../database/usersdata';
+
 
 export const LoginForm: React.FC = () => {
+  const [users, setEventos] = useState<User[]>(usersData);
+
   // Estado para almacenar el nombre de usuario y contraseña
   const [formData, setFormData] = useState({
     username: '',
     password: ''
   });
+
+  const [errorMessage, setErrorMessage] = useState(''); // Para mostrar mensajes de error
+
+  // Hook para redirigir
+  const navigate = useNavigate();
 
   // Manejador para actualizar los valores del formulario
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,8 +30,25 @@ export const LoginForm: React.FC = () => {
   // Manejador para enviar el formulario
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Aquí puedes guardar los datos, enviarlos a una API, etc.
-    console.log('Datos ingresados:', formData);
+
+    // Buscar el usuario en el array
+    const user = users.find(u => u.user_name === formData.username && u.contrasena === formData.password);
+
+    if (user) {
+      // Si el usuario existe, redirige dependiendo de la membresía
+      if (user.miembro === 1) {
+        navigate('/page1'); // Redirige a la página 1
+      } else if (user.miembro === 2) {
+        navigate('/page2'); // Redirige a la página 2
+      } else if (user.miembro === 3) {
+        navigate('/page3'); // Redirige a la página 3
+      } else if (user.miembro === 4) {
+        navigate('/AdminDashboard'); // Redirige a la página 3
+      }
+    } else {
+      // Si no encuentra al usuario, muestra un mensaje de error
+      setErrorMessage('Usuario o contraseña incorrectos');
+    }
   };
 
   return (
@@ -47,6 +75,9 @@ export const LoginForm: React.FC = () => {
           required
         />
       </div>
+
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} {/* Muestra el error si existe */}
+
       <button type="submit">Iniciar sesión</button>
     </form>
   );
